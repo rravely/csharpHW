d1e03a9e6156dec59f6c7e67977e6e188963499a
rahyun <116885638+rravely@users.noreply.github.com>
2023-03-22 17:41:12 +0900

    change seat

---
+++ 20230322_homeWork_sitting/20230322_homeWork_sitting/Student_info.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student_info
+    {
+        public string Name;
+        public string Gender;
+        public string PhoneNumber;
+
+        public Student_info(string Name, string Gender, string PhoneNumber)
+        {
+            this.Name = Name;
+            this.Gender = Gender;
+            this.PhoneNumber = PhoneNumber;
+        }
+
+    }
+}
